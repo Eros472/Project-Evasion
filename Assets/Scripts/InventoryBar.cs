8c2f575588761003a9a3fe7afe2394d153dfeca9
Eros472 <162504696+Eros472@users.noreply.github.com>
2025-05-12 15:45:50 -0700

    Revert "Merge branch 'branch3' of https://github.com/Eros472/Project-Evasion into branch3"
    This reverts commit 70bcc62135c4cec5031c67957aca5d4dfb5fd54a, reversing
    changes made to 7d05c06ebbdfa88b3abc5ccb8668632e29941315.

--- Assets/Scripts/InventoryBar.cs
+++ Assets/Scripts/InventoryBar.cs
@@ -3,107 +3,41 @@ using UnityEngine.UI;
 
 public class InventoryBar : MonoBehaviour
 {
-    public Image[] slotImages;
-    public Text[] quantityTexts;
+    public Image[] slots; // Assign 4 slot Images in Inspector
     public Color normalColor = Color.white;
     public Color selectedColor = Color.yellow;
 
-    private void Start()
+    private int currentIndex = 0;
+
+    void Start()
     {
-        UpdateUI();
+        UpdateSlotHighlight();
     }
 
-    private void Update()
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1)) SelectSlot(0);
         if (Input.GetKeyDown(KeyCode.Alpha2)) SelectSlot(1);
         if (Input.GetKeyDown(KeyCode.Alpha3)) SelectSlot(2);
         if (Input.GetKeyDown(KeyCode.Alpha4)) SelectSlot(3);
-
-        if (Input.GetKeyDown(KeyCode.Space)) UseSelectedItem();
     }
 
-    private void SelectSlot(int index)
+    void SelectSlot(int index)
     {
-        GameManager.Instance.SetSelectedSlot(index);
-        UpdateUI();
+        currentIndex = index;
+        UpdateSlotHighlight();
     }
 
-    private void UpdateUI()
+    void UpdateSlotHighlight()
     {
-        for (int i = 0; i < slotImages.Length; i++)
+        for (int i = 0; i < slots.Length; i++)
         {
-            slotImages[i].color = (i == GameManager.Instance.GetSelectedSlot()) ? selectedColor : normalColor;
-
-            var slot = GameManager.Instance.GetInventorySlot(i);
-            quantityTexts[i].text = (slot.IsStackable && slot.quantity > 0)
-                ? slot.quantity.ToString() : "";
+            slots[i].color = (i == currentIndex) ? selectedColor : normalColor;
         }
     }
 
-    public void PickupItem(InventoryItemType itemType, int amount)
+    public int GetSelectedSlot()
     {
-        var inventory = GameManager.Instance.inventory;
-
-        if (itemType == InventoryItemType.HealthPack)
-        {
-            for (int i = 0; i < inventory.Length; i++)
-            {
-                if (inventory[i].itemType == InventoryItemType.HealthPack && inventory[i].quantity < 10)
-                {
-                    inventory[i].quantity = Mathf.Min(inventory[i].quantity + amount, 10);
-                    UpdateUI();
-                    return;
-                }
-            }
-        }
-
-        for (int i = 0; i < inventory.Length; i++)
-        {
-            if (inventory[i].itemType == InventoryItemType.None)
-            {
-                inventory[i].itemType = itemType;
-                inventory[i].quantity = amount;
-                UpdateUI();
-                return;
-            }
-        }
-
-        Debug.Log("No space to add item: " + itemType);
+        return currentIndex;
     }
-
-    private void UseSelectedItem()
-    {
-        int index = GameManager.Instance.GetSelectedSlot();
-        var slot = GameManager.Instance.GetInventorySlot(index);
-
-        if (slot.itemType == InventoryItemType.HealthPack && slot.quantity > 0)
-        {
-            Player player = FindObjectOfType<Player>();
-            player.Heal(2);
-
-            slot.quantity--;
-            if (slot.quantity <= 0)
-                slot.itemType = InventoryItemType.None;
-
-            UpdateUI();
-        }
-        else if (slot.itemType == InventoryItemType.Key)
-        {
-            Door door = FindObjectOfType<Door>();
-            if (door != null && door.IsPlayerInRange())
-            {
-                door.Unlock();
-
-                slot.quantity--;
-                if (slot.quantity <= 0)
-                    slot.itemType = InventoryItemType.None;
-
-                UpdateUI();
-            }
-        }
-    }
-
-    public InventorySlot GetSelectedSlot() => GameManager.Instance.GetInventorySlot(GameManager.Instance.GetSelectedSlot());
-    public InventoryItemType GetSelectedItemType() => GetSelectedSlot().itemType;
 }
